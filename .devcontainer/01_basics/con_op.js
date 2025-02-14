29f3145e847f37699e30a1518a30e5b7b739c864
withayush <140087615+withayush@users.noreply.github.com>
2025-02-14 13:10:55 +0000

    con_op

--- .devcontainer/01_basics/con_op.js
+++ .devcontainer/01_basics/con_op.js
@@ -7,8 +7,28 @@
 //  console.log(typeof valueInNumber);
 //  console.log(valueInNumber);
 
-let  isLoggedIn= 1 
+// let  isLoggedIn= 1 
 
-let booleanIsLoggedin = Boolean(isLoggedIn)
+// let booleanIsLoggedin = Boolean(isLoggedIn)
 
-console.log(booleanIsLoggedin)
+// console.log(booleanIsLoggedin)
+
+// let value = 3
+// let negvalue = -value
+
+// console.log(negvalue)
+// console.log(typeof negvalue)
+
+
+// console.log("1" + "2")
+// console.log(1 + "2")
+// console.log("1" + 2 + 2)
+// console.log(1+2+"5")
+
+// console.log(true);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+console.log("2" === 2);
